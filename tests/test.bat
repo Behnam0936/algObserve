b853faf5816fca5da6ecc555619e4e7065ee4441
Zsákai Bálint <ruphyy@inf.elte.hu>
2021-10-27 22:00:26 +0200

    added manual tests

---
+++ tests/test.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+ECHO =======================
+ECHO Testing the project...
+ECHO =======================
+g++ -o test test.cpp
+.\test
+ECHO =======================
+ECHO Tests compiled
+ECHO =======================
+pause
